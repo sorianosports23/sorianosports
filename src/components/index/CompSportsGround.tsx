e035557287c1183db8b31433de6696ccd3e58aff
Efra <efrainschettini.123@gmail.com>
2023-07-23 19:14:55 -0300

    noticias tierra y acuaticas funcionales

--- src/components/index/CompSportsGround.tsx
+++ src/components/index/CompSportsGround.tsx
@@ -1,26 +1,65 @@
+import { FaBasketballBall, FaVolleyballBall } from "react-icons/fa"
 import styles from "../../assets/css/index/sportsground.module.css"
+import { useState } from "react"
 
 const CompSportsGround = () => {
-  return (
-    <div className={styles.sportsmain}>
-      <div className={styles.textDiv}>
-        <div>
-          <h2>
-            <span>Deportes</span>
-            <span>Actividades</span>
-            <span>En tierra</span>
-          </h2>
-          <p>Simplemente juega. Diviértete. Disfruta el juego.</p>
-        </div>
-        <div className={styles.img}>
-          <img src={process.env.PUBLIC_URL + "/assets/img/sportsground.jpg"} alt="sports-ground" />
-        </div>
-      </div>
-      <div>
-        <button>Abrir</button>
-      </div>
 
-      <div className={styles.circle}></div>
+  const [listOpen, setListOpen] = useState(false)
+
+  return (
+    <div 
+      className={styles.sportsmain}
+      style={{
+        height: listOpen ? "40rem" : "25rem"
+      }}
+    >
+      <div className={styles["sportsmain_main"]}>
+        <div className={styles.textDiv}>
+          <div>
+            <h2>
+              <span>Deportes</span>
+              <span>Actividades</span>
+              <span>En tierra</span>
+            </h2>
+            <p>Simplemente juega. Diviértete. Disfruta el juego.</p>
+          </div>
+          <div className={styles.img}>
+            <img src={process.env.PUBLIC_URL + "/assets/img/sportsground.jpg"} alt="sports-ground" />
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={() => setListOpen(!listOpen)}
+          >
+            {
+              listOpen ? "Cerrar" : "Abrir"
+            }
+          </button>
+        </div>
+
+        <div className={styles.circle}></div>
+      </div>
+      
+      <div className={styles.list}>
+        <ul>
+          <li>
+            <div className={styles.icon}>
+              <FaVolleyballBall/>
+            </div>
+            <div className={styles.title}>
+              Voley
+            </div>
+          </li>
+          <li>
+            <div className={styles.icon}>
+              <FaBasketballBall/>
+            </div>
+            <div className={styles.title}>
+              Basquetbol
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/index/CompSportsWater.tsx
+++ src/components/index/CompSportsWater.tsx
@@ -1,26 +1,65 @@
+import { BsQuestionCircle } from "react-icons/bs"
 import styles from "../../assets/css/index/sportswater.module.css"
+import { useState } from "react"
 
 const CompSportsWater = () => {
+
+  const [listOpen, setListOpen] = useState(false)
+
   return (
-    <div className={styles.sportsmain}>
-      <div className={styles.textDiv}>
-        <div className={styles.img}>
-          <img src={process.env.PUBLIC_URL + "/assets/img/sportswater.jfif"} alt="sports-ground" />
+    <div 
+      className={styles.sportsmain}
+      style={{
+        height: listOpen ? "40rem" : "25rem"
+      }}
+    >
+      <div className={styles["sportsmain_main"]}>
+        <div className={styles.textDiv}>
+          <div className={styles.img}>
+            <img src={process.env.PUBLIC_URL + "/assets/img/sportswater.jfif"} alt="sports-ground" />
+          </div>
+          <div>
+            <h2>
+              <span>Deportes</span>
+              <span>Actividades</span>
+              <span>Acuaticas</span>
+            </h2>
+            <p>Recuerda hacer las cosas que disfrutas.</p>
+          </div>
         </div>
         <div>
-          <h2>
-            <span>Deportes</span>
-            <span>Actividades</span>
-            <span>Acuaticas</span>
-          </h2>
-          <p>Recuerda hacer las cosas que disfrutas.</p>
+          <button
+            onClick={() => setListOpen(!listOpen)}
+          >
+            {
+              listOpen ? "Cerrar" : "Abrir"
+            }
+          </button>
         </div>
-      </div>
-      <div>
-        <button>Abrir</button>
+
+        <div className={styles.circle}></div>
       </div>
 
-      <div className={styles.circle}></div>
+      <div className={styles.list}>
+        <ul>
+          <li>
+            <div className={styles.icon}>
+              <BsQuestionCircle/>
+            </div>
+            <div className={styles.title}>
+              ????????
+            </div>
+          </li>
+          <li>
+            <div className={styles.icon}>
+              <BsQuestionCircle/>              
+            </div>
+            <div className={styles.title}>
+              ????????
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )  
 }
