6083d260b2392f2da0919fe0f466fecdd5aa4889
efrask7 <efrainschettini.123@gmail.com>
2023-08-02 12:56:45 -0300

    eventos .2

--- src/pages/user/PageEvents.tsx
+++ src/pages/user/PageEvents.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
 import CompEvent from "../../components/events/CompEvent"
 import CompHeader from "../../components/templates/CompHeader"
 import styles from "../../css/events/main.module.css"
-import { BsFillCaretDownFill, BsFillCalendarDateFill } from "react-icons/bs"
+import { BsFillCaretDownFill, BsFillCalendarDateFill , BsFillCaretUpFill} from "react-icons/bs"
 
 const PageEvents = () => {
+
+  const [showPlaces, setShowPlaces] = useState(false)
+
   return (
     <>
     <CompHeader/>
@@ -13,10 +17,35 @@ const PageEvents = () => {
         <div className={styles.title}>
           <h1>Programación</h1>
 
-          <div>
+          <div className={styles.btn_cont}>
             <div>
-              <button>Localidades <BsFillCaretDownFill/></button>
-              <p>Mercedes</p>
+              <button
+                onClick={() => setShowPlaces(!showPlaces)}
+              >
+                Localidades
+                {
+                  showPlaces
+                    ? <BsFillCaretDownFill/>
+                    : <BsFillCaretUpFill/>
+                }
+              </button>
+              <p
+                style={{
+                  borderRadius: showPlaces
+                    ? "0" 
+                    : "0 0 1rem 1rem"
+                }}
+              >Mercedes</p>
+
+              <ul className={styles.btn_cont_list}
+                style={{
+                  display: showPlaces ? "block" : "none"
+                }}
+              >
+                <li>Dolores</li>
+                <li>Risso</li>
+                <li>Palmitas</li>
+              </ul>
             </div>
 
             <button>Fecha <BsFillCalendarDateFill/></button>
@@ -24,6 +53,55 @@ const PageEvents = () => {
         </div>
 
         <div className={styles.events}>
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
+          <CompEvent
+            dayNumber={8}
+            day={"SAB"}
+            hour={"12:00 H"}
+            sportName="REMO"
+            place="Club Remeros Mercedes"
+          />
           <CompEvent
             dayNumber={8}
             day={"SAB"}
