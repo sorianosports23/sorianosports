f1b726d9367c42a5e08f63635a100f2d63a0da92
efrask7 <efrainschettini.123@gmail.com>
2023-08-21 11:10:28 -0300

    componente general para las paginas de admin

--- src/pages/admin/PageIndexAdmin.tsx
+++ src/pages/admin/PageIndexAdmin.tsx
@@ -1,27 +1,11 @@
-import { Link } from "react-router-dom"
-import { BsFillHouseFill } from "react-icons/bs"
 import CompDashboard from "../../components/admin/index/CompDashboard"
-import CompHeaderAdmin from "../../components/admin/templates/CompHeaderAdmin"
-import styles from "../../css/admin/index/PageAdmin.module.css"
+import PageAdmin from "./PageAdmin"
 
 const PageIndexAdmin = () => {
   return (
-    <>
-    <CompHeaderAdmin/>
-
-    <main className={styles.main}>
-      <header className={styles.header}>
-        <h1>Inicio</h1>
-
-        <div className={styles.header_btns}>
-          <Link to="/"><BsFillHouseFill/> <p>Pagina principal</p></Link>
-        </div>
-      </header>
-      <div className={styles.content}>
-        <CompDashboard/>
-      </div>
-    </main>
-    </>
+    <PageAdmin>
+      <CompDashboard/>
+    </PageAdmin>
   )
 }
 
