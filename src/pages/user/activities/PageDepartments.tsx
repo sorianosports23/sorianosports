b2c2ccc5bca04b0aed0cd625fbd99e3bdea1fd38
Efra <efrainschettini.123@gmail.com>
2023-08-21 20:46:35 -0300

    mapa ejemplo :)

--- src/pages/user/activities/PageDepartments.tsx
+++ src/pages/user/activities/PageDepartments.tsx
@@ -1,10 +1,13 @@
 import PageUser from "../PageUser"
-
+import styles from "../../../css/activities/departments/PageDepartments.module.css"
+import CompDepartmentsMap from "../../../components/templates/CompDepartmentsMap"
 
 const PageDepartments = () => {
   return (
     <PageUser>
-      
+      <div className={styles.content} id="test">
+        <CompDepartmentsMap/>
+      </div>
     </PageUser>
   )
 }
