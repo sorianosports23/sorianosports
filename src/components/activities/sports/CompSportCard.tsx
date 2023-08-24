e26b8c76503fd4a361d40e6eb797b1d0ddc4d371
Efra <efrainschettini.123@gmail.com>
2023-08-23 21:19:19 -0300

    pagina de deportes funcionalidad

---
+++ src/components/activities/sports/CompSportCard.tsx
@@ -0,0 +1,32 @@
+import styles from "../../../css/activities/sports/CompSportCard.module.css"
+
+type TCardProps = {
+  name: string
+  iconUrlName: string
+  backgroundUrlName: string
+}
+
+const CompSportCard = ({ name, iconUrlName, backgroundUrlName }: TCardProps) => {
+
+  const iconImage = process.env.PUBLIC_URL + "/assets/img/icons/" + iconUrlName
+  const backgroundImage = process.env.PUBLIC_URL + "/assets/img/cards/" + backgroundUrlName
+
+  return (
+    <div 
+      className={styles.sportCard}
+      style={{
+        backgroundImage: `url(${backgroundImage})`
+      }}
+    >
+      <div>
+        <p>{name}</p>
+
+        <div className={styles.sportIcon}>
+          <img src={iconImage} alt={name} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CompSportCard
--- src/pages/user/activities/PageSports.tsx
+++ src/pages/user/activities/PageSports.tsx
@@ -1,40 +1,68 @@
 import PageUser from "../PageUser"
 import styles from "../../../css/activities/sports/PageSports.module.css"
 import CompContainer from "../../../components/templates/CompContainer"
+import CompSportCard from "../../../components/activities/sports/CompSportCard"
+import { useState } from "react"
+
+const sports = {
+  ground: [
+    { name: "Basquetbol", icon: "basketball.png", background: "basketball.jfif" },
+    { name: "Boxeo", icon: "box.png", background: "box.jfif" }
+  ],
+  water: [
+    { name: "Remo", icon: "remo.png", background: "remo.png" }
+  ]
+}
+
+type TEventType = "ground" | "water" | undefined
 
 const PageSports = () => {
+
+  const [eventTypeSelected, setEventTypeSelected] = useState<TEventType>(undefined)
+
   return (
     <PageUser>
       <CompContainer>
         <div className={styles.selector}>
-          <div className={styles.ground}>
+          <button 
+            className={styles.ground}
+            onClick={() => setEventTypeSelected("ground")}
+          >
             <div className={styles.icon}>
               <img src={process.env.PUBLIC_URL + "/assets/img/icons/running.png"} alt="person-running" />
             </div>
             <p>
               Deportes terrestres
             </p>
-          </div>
+          </button>
           <div className={styles.separator}>
             <img src={process.env.PUBLIC_URL + "/assets/img/secretaria_deportes.svg"} alt="deportes" />
           </div>
-          <div className={styles.water}>
+          <button 
+            className={styles.water}
+            onClick={() => setEventTypeSelected("water")}
+          >
             <div className={styles.icon}>
               <img src={process.env.PUBLIC_URL + "/assets/img/icons/swimming.png"} alt="person-swimming" />
             </div>
             <p>
               Deportes acuaticos
             </p>
-          </div>
+          </button>
         </div>
 
         <div className={styles.sports}>
-          <div className={styles.sportCard}>
-            <div className={styles.sportIcon}>
-              <img src={process.env.PUBLIC_URL + "/assets/img/icons/basketball.png"} alt="basketball" />
-            </div>
-            <p>Basquetbol</p>
-          </div>
+          {
+            eventTypeSelected 
+              && sports[eventTypeSelected as keyof typeof sports].map((sport, i) => (
+                <CompSportCard
+                  name={sport.name}
+                  iconUrlName={sport.icon}
+                  backgroundUrlName={sport.background}
+                  key={i}
+                />
+              ))
+          }
         </div>
       </CompContainer>
     </PageUser>
