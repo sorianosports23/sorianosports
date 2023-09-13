3ae06e337e5e82d60d19a9414a34135cbcb0d0d2
Efra <efrainschettini.123@gmail.com>
2023-09-13 15:45:46 -0300

    colores + arreglo de diseño

---
+++ src/components/index/SportsGround.tsx
@@ -0,0 +1,68 @@
+import { FaBasketballBall, FaVolleyballBall } from "react-icons/fa"
+import styles from "../../css/index/sportsground.module.css"
+import { useState } from "react"
+import assetsFolder from "../../utils/publicfolder"
+
+const SportsGround = () => {
+
+  const [listOpen, setListOpen] = useState(false)
+
+  return (
+    <div 
+      className={styles.sportsmain}
+      style={{
+        height: listOpen ? "40rem" : "25rem"
+      }}
+    >
+      <div className={styles["sportsmain_main"]}>
+        <div className={styles.textDiv}>
+          <div>
+            <h2>
+              <span>Actividades</span>
+              <span>Deportivas</span>
+              <span>Anuales</span>
+            </h2>
+              <p>¡El deporte es vida... Vivi el deporte!</p>
+              <button
+                onClick={() => setListOpen(!listOpen)}
+              >
+              {
+                listOpen ? "Cerrar" : "Abrir"
+              }
+            </button>
+
+           
+          </div>
+          <div className={styles.img}>
+            <img src={assetsFolder + "/img/sportsground.jpg"} alt="sports-ground" />
+          </div>
+        </div>
+        
+        <div className={styles.circle} data-ground-open={listOpen}></div>
+      </div>
+      
+      <div className={styles.list}>
+        <ul>
+          <li>
+            <div className={styles.icon}>
+              <FaVolleyballBall/>
+            </div>
+            <div className={styles.title}>
+              Voley
+            </div>
+          </li>
+          <li>
+            <div className={styles.icon}>
+              <FaBasketballBall/>
+            </div>
+            <div className={styles.title}>
+              Basquetbol
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default SportsGround
